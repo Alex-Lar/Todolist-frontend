72ba96a09e8b5f1a43d08d9a9ab622036b2cc958
Alexander Larin <doremilarin@gmail.com>
2023-06-18 15:34:13 +0300

    update: html layout, css styles. add: filters-popover script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    "prettier",
-  ],
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   env: {
     browser: true,
     node: true,
+    es6: true,
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("Hello World!");
+import "./public/scripts/filters-popover.js";
+
