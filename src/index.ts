b0be8af877bdb1704ebf903ddb0a1b86fee569ab
Alexander Larin <doremilarin@gmail.com>
2023-08-20 21:13:19 +0300

    Update index.ts imports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+// Fonts
+import './public/fonts/Poppins-Bold.ttf';
+import './public/fonts/Poppins-Regular.ttf';
+
+// CSS files
+import './public/css/reset.css';
+import './public/css/styles.css';
+
+// HTML files
+import './index.html';
+
+// Images
+import './public/img/avatar.png';
+
+// Typescript
 import './app.ts';
+import './public/scripts/filters-popover.ts';
